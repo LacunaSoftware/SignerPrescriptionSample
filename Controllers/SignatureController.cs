01b87b0ce9d0778df8ea3e10c214c52224996cbc
Daniel Rufino <daniel.s.rufino@gmail.com>
2022-05-06 15:17:52 -0300

    Initial Commit

---
+++ Controllers/SignatureController.cs
@@ -0,0 +1,30 @@
+﻿
+using Lacuna.Signer.Api;
+using Lacuna.Signer.Api.Documents;
+using Lacuna.Signer.Api.FlowActions;
+using Lacuna.Signer.Api.Users;
+using Lacuna.Signer.Client;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Embedded_Signatures.Controllers
+{
+    public class SignatureController : Controller
+    {
+
+     
+        public async Task<IActionResult> Index()
+        {
+            // 1. The file's bytes must be read by the application and uploaded
+            var filePath = "sample.pdf";
+            var fileName = Path.GetFileName(filePath);
+            var file = File.ReadAllBytes(filePath);
+            var uploadModel = await SignerClient.UploadFileAsync(fileName, file, "application/pdf");
+
+            // 2. Define the name of the document which will be visible in the application
+            var fileUploadModel = new FileUploadModel(uploadModel) { DisplayName = "Embedded Signature Sample" };
+        }
+       
+
+    }
+}

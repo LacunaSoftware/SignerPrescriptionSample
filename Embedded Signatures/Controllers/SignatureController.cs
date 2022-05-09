6cfc0b1479ee43960ad0ed74730c12cf9ce94421
Danilo Bispo <danilobispogalvao@gmail.com>
2022-05-09 11:16:07 -0300

    minhas alterações

--- Embedded Signatures/Controllers/SignatureController.cs
+++ Embedded Signatures/Controllers/SignatureController.cs
@@ -14,18 +14,13 @@ namespace Embedded_Signatures.Controllers
 {
     public class SignatureController : Controller
     {
-
+        
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var SignerClient = new SignerClient("https://signer-lac.azurewebsites.net", "API Sample App|43fc0da834e48b4b840fd6e8c37196cf29f919e5daedba0f1a5ec17406c13a99");
-            var filePath = "sample.pdf";
-            var fileName = Path.GetFileName(filePath);
-            var file = System.IO.File.ReadAllBytes(filePath);
-            var uploadModel = await SignerClient.UploadFileAsync(fileName,file, "application/pdf");
-            var fileUploadModel = new FileUploadModel(uploadModel) { DisplayName = "Embedded Signature Sample" };
+            ViewData["endpoint"] = "https://signer-lac.azurewebsites.net";
+            ViewData["token"] = "API Sample App|43fc0da834e48b4b840fd6e8c37196cf29f919e5daedba0f1a5ec17406c13a99";
 
-      
             var participantUser = new ParticipantUserModel()
             {
                 Name = "Jack Bauer",
@@ -39,24 +34,34 @@ namespace Embedded_Signatures.Controllers
             };
 
             
-            var documentRequest = new CreateDocumentRequest()
-            {
-                Files = new List<FileUploadModel>() { fileUploadModel },
-                FlowActions = new List<FlowActionCreateModel>() { flowActionCreateModel }
-            };
-            var result = (await SignerClient.CreateDocumentAsync(documentRequest)).First();
-            var actionUrlRequest = new ActionUrlRequest()
-            {
-                Identifier = participantUser.Identifier
-            };
-            var actionUrlResponse = await SignerClient.GetActionUrlAsync(result.DocumentId, actionUrlRequest);
+            //var documentRequest = new CreateDocumentRequest()
+            //{
+            //    Files = new List<FileUploadModel>() { fileUploadModel },
+            //    FlowActions = new List<FlowActionCreateModel>() { flowActionCreateModel }
+            //};
+            //var result = (await SignerClient.CreateDocumentAsync(documentRequest)).First();
+            //var actionUrlRequest = new ActionUrlRequest()
+            //{
+            //    Identifier = participantUser.Identifier
+            //};
+            //var actionUrlResponse = await SignerClient.GetActionUrlAsync(result.DocumentId, actionUrlRequest);
+            //ViewData["response"] = actionUrlResponse.EmbedUrl; 
 
-
-            Console.WriteLine(actionUrlResponse.EmbedUrl);
+            //Console.WriteLine(actionUrlResponse.EmbedUrl);
 
             return View();
         }
       
-
+        private async Task<FileUploadModel> uploadSampleFile(string endpoint, string token)
+        {
+           
+            var SignerClient = new SignerClient(endpoint, token);
+            var filePath = "sample.pdf";
+            var fileName = Path.GetFileName(filePath);
+            var file = System.IO.File.ReadAllBytes(filePath);
+            var uploadModel = await SignerClient.UploadFileAsync(fileName, file, "application/pdf");
+            var fileUploadModel = new FileUploadModel(uploadModel) { DisplayName = "Embedded Signature Sample" };
+            return fileUploadModel;
+        }
     }
 }

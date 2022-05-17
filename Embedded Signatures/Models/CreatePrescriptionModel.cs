76cd46179a9204dbc747d9f4193417c2d2bc2033
Daniel Rufino <daniel.s.rufino@gmail.com>
2022-05-17 09:57:46 -0300

    Removed unnecessary

--- Embedded Signatures/Models/CreatePrescriptionModel.cs
+++ Embedded Signatures/Models/CreatePrescriptionModel.cs
@@ -5,10 +5,5 @@ namespace Embedded_Signatures.Models
     {
         public string PatientName { get; set; }
         public string MedicationName { get; set; }
-
-        public string? EmbedUrl { get; set; }
-
-        public string? JavascriptToRun { get; set; }
-   
     }
 }

4ef30209239a1f0515dc5ab80d2d53b4afe264d6
Daniel Rufino <daniel.s.rufino@gmail.com>
2022-05-17 09:57:58 -0300

    Revert "Removed unnecessary"
    This reverts commit 76cd46179a9204dbc747d9f4193417c2d2bc2033.

--- Embedded Signatures/Models/CreatePrescriptionModel.cs
+++ Embedded Signatures/Models/CreatePrescriptionModel.cs
@@ -5,5 +5,10 @@ namespace Embedded_Signatures.Models
     {
         public string PatientName { get; set; }
         public string MedicationName { get; set; }
+
+        public string? EmbedUrl { get; set; }
+
+        public string? JavascriptToRun { get; set; }
+   
     }
 }

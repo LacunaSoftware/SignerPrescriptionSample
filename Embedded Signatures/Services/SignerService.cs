2094bdea55d75ba6d0492c3ee95854d50fb433bb
Daniel Rufino <daniel.s.rufino@gmail.com>
2022-06-01 18:48:18 -0300

    Working sample + UI changes

--- Embedded Signatures/Services/SignerService.cs
+++ Embedded Signatures/Services/SignerService.cs
@@ -23,7 +23,7 @@ namespace Embedded_Signatures.Services
             client = new SignerClient(url, "API Sample App|43fc0da834e48b4b840fd6e8c37196cf29f919e5daedba0f1a5ec17406c13a99");
         }
 
-        public async Task<string> CreateDocument(string namePaciente, string medicine,string identifier, bool allowElectronicSignature = false)
+        public async Task<string> CreateDocument(string namePaciente, string medicine, string identifier, bool allowElectronicSignature = false)
         {
             var fileStream = CreatePrescriptionPdf(namePaciente, medicine);
             var filePath = "Template-Prescricao.pdf";
@@ -33,11 +33,10 @@ namespace Embedded_Signatures.Services
 
             var participantUser = new ParticipantUserModel()
             {
-                Name = "Pierre de Fermat"
+                Name = "Pierre de Fermat",
+                Email = "test@lacunasoftware.com"
             };
             participantUser.Identifier = identifier;    
-       
-
           
             var flowActionCreateModel = new FlowActionCreateModel()
             {

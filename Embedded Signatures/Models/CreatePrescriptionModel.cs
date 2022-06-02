286206ac20f24d6c64eb8d52bc6ffa000cd15d17
Daniel Rufino <daniel.s.rufino@gmail.com>
2022-06-02 17:40:31 -0300

    Working sample but still need refactor

--- Embedded Signatures/Models/CreatePrescriptionModel.cs
+++ Embedded Signatures/Models/CreatePrescriptionModel.cs
@@ -3,7 +3,9 @@ namespace Embedded_Signatures.Models
 {
     public class CreatePrescriptionModel
     {
-        public string Identifier{ get; set; } 
+        public string Identifier { get; set; }
+        public string? CertifiedName { get; set; }
+        public string Email { get; set; }
         public string PatientName { get; set; }
         public string MedicationName { get; set; }
         public bool AllowElectronicSignature { get; set; }
--- Embedded Signatures/Services/SignerService.cs
+++ Embedded Signatures/Services/SignerService.cs
@@ -23,7 +23,7 @@ namespace Embedded_Signatures.Services
             client = new SignerClient(url, "API Sample App|43fc0da834e48b4b840fd6e8c37196cf29f919e5daedba0f1a5ec17406c13a99");
         }
 
-        public async Task<string> CreateDocument(string namePaciente, string medicine, string identifier, bool allowElectronicSignature = false)
+        public async Task<string> CreateDocument(string namePaciente, string certifiedName, string email, string medicine, string identifier, bool allowElectronicSignature = false)
         {
             var fileStream = CreatePrescriptionPdf(namePaciente, medicine);
             var filePath = "Template-Prescricao.pdf";
@@ -31,11 +31,10 @@ namespace Embedded_Signatures.Services
             var uploadModel = await client.UploadFileAsync(fileName, fileStream, "application/pdf");
             var fileUploadModel = new FileUploadModel(uploadModel) { DisplayName = "Embedded Signature Sample" };
 
-            var participantUser = new ParticipantUserModel()
-            {
-                Name = "Pierre de Fermat",
-                Email = "test@lacunasoftware.com"
-            };
+            var participantUser = new ParticipantUserModel();
+
+            participantUser.Email = email;
+            participantUser.Name = certifiedName;
             participantUser.Identifier = identifier;    
           
             var flowActionCreateModel = new FlowActionCreateModel()
